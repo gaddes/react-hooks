0d3c9259c798e0e8ad3f0fa4248e5365247b4404
Matthew Gaddes <matthew.gaddes@gmail.com>
2022-01-23 06:35:15 -0800

    Complete 04 extra cred 1 & 2

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,11 +2,11 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
-import { useState } from 'react';
+import { useLocalStorageState } from '../utils';
 
 function Board() {
   // MANAGED state
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useLocalStorageState('squares', Array(9).fill(null));
 
   // DERIVED state
   const nextValue = calculateNextValue(squares);
